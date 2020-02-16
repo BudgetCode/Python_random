c409b2d595ddf0269a9cd2d0068634cde093dcf5
Anurag Singh <30367300+BudgetCode@users.noreply.github.com>
2020-02-16 20:37:10 +0530

    Add files via upload

---
+++ pizza2.py
@@ -0,0 +1,20 @@
+def subset_sum(nos, smax, top,res=[],pi=[],n=0,d=-1):
+    s = sum(res)
+    if s <= smax and s > n:
+        n = s
+        yield [s , pi]
+    if s >= smax:
+        return  
+    for i in range(len(nos)):
+        n = nos[i]
+        d+=1
+        rm = nos[i+1:]
+        yield from subset_sum(rm, smax,top, res + [n],pi+[d],n,d) 
+
+
+if __name__ == "__main__":
+    k , t = list(map(int,input().split()))
+    lis = list(map(int,input().split()))
+    p = max(list(subset_sum(lis,k,lis)),key=lambda x:x[0])
+    print(len(p[1]))
+    print(" ".join(map(str,p[1])))
